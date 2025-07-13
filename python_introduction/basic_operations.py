be78dd2d988356f7ce34484b913be583547103ea
Lina Ahmed <linaahmed541@gmail.com>
2025-07-13 09:36:23 +0300

    updated basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1+number2
+difference = number1-number2
+multiplication = number1*number2
+print(f"Your sum is {sum}")
+print(f"Your difference is {difference}")
+print(f"The multiplication result is {multiplication}")
