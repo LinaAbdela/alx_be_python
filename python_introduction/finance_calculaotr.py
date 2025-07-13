10cf2abf6a4f6570b0a1a90fb521bf0cd5735653
Lina Ahmed <linaahmed541@gmail.com>
2025-07-13 14:07:14 +0300

    commited

--- python_introduction/finance_calculaotr.py
+++ python_introduction/finance_calculaotr.py
@@ -1,7 +1,7 @@
-income = int(input("Enter your monthly income:"))
+Monthly_income = int(input("Enter your monthly income:"))
 total_monthly_expenses = int(input("Enter yout  total monthly expenses:"))
-monthly_savings = income - total_monthly_expenses
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print(f"your monthly savings are {monthly_savings}")
+Monthly_Savings = Monthly_income - total_monthly_expenses
+Projected_Savings = Monthly_Savings * 12 * 0.05
+print(f"your monthly savings are {Monthly_Savings}")
 print(
-    f"your the projected annual savings after including interest are {projected_savings}")
+    f"projected savings after one year, with interest, is {projected_savings}")
