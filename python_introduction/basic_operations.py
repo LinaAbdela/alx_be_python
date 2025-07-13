12b68adb505fac7c388635892d791e8c1d9830a2
Lina Ahmed <linaahmed541@gmail.com>
2025-07-13 09:46:15 +0300

    here is 3rd update

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 sum = number1+number2
 difference = number1-number2
 multiplication = number1*number2
-print(f"Your sum is {sum}")
-print(f"Your difference is {difference}")
-print(f"The multiplication result is {multiplication}")
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtrationtion of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
